3cf6f0a9b92e6b8472d1aeaf46ec2c743c5d6d1e
ecxal <ecxalcar0612@gmail.com>
2020-06-23 19:50:16 -0600

    finished page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,17 +2,41 @@ $(document).ready(function(){
 	$("#btn-menu").click(function() {
         $(this).toggleClass("open");
     });
-	
+
     $("#btn-menu").click(function() {
 		$(".bg-nav").slideToggle();
 	});
-	
+
+	// ==============Menu Globally available options==============
+    $('ul li').click(function(){
+        // alert(id);
+        $('li').removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $('ul li a').click(function(e){
+        e.preventDefault();
+        var id = $(this).attr('data-id');
+        goToByScroll(id);
+        // var url = '?option=' + id;
+        // history.pushState({}, null, url);
+	});
+
+	function goToByScroll(id) {
+        // Remove "link" from the ID
+        id = id.replace("link", "");
+		var header = $('header').height();
+		console.log(header);
+        // Scroll
+        $('html,body').animate({
+            scrollTop: $("#" + id).offset().top - header
+        }, 'slow');
+    }
+
     $(".bg-nav .container ul li a").click(function() {
 		$(".bg-nav").slideUp();
 		$('#btn-menu').removeClass("open");
 	});
-	
-
 
 	$('.single-item').slick({
 		arrows: false,
