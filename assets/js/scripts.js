251511634f76ff042eacadf4d0ec819b9a5b0e82
ecxal <ecxalcar0612@gmail.com>
2020-06-23 18:44:15 -0600

    page finished

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,10 +2,17 @@ $(document).ready(function(){
 	$("#btn-menu").click(function() {
         $(this).toggleClass("open");
     });
-
+	
     $("#btn-menu").click(function() {
-        $(".bg-nav").slideToggle();
-    });
+		$(".bg-nav").slideToggle();
+	});
+	
+    $(".bg-nav .container ul li a").click(function() {
+		$(".bg-nav").slideUp();
+		$('#btn-menu').removeClass("open");
+	});
+	
+
 
 	$('.single-item').slick({
 		arrows: false,
